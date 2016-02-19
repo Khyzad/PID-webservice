cf6159bba3c287b8ba65f318996ffe0c963b4687
Khyzad <lljruffin@gmail.com>
2016-02-18 17:37:04 -1000

    Add PidDaoImpl to dao
    This class implements PidDao and declares it as a bean so that the
    latter of which can be turned into a Spring Bean.

---
+++ Minter/src/main/java/com/hida/dao/PidDaoImpl.java
@@ -0,0 +1,62 @@
+package com.hida.dao;
+
+import com.hida.model.Pid;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author lruffin
+ */
+@Repository("pidDao")
+public class PidDaoImpl extends AbstractDao<String, Pid> implements PidDao {
+
+    /**
+     * missing javadoc
+     *
+     * @param name
+     * @return
+     */
+    @Override
+    public Pid findByName(String name) {
+        return getByKey(name);
+    }
+
+    /**
+     * missing javadoc
+     *
+     * @param pid
+     */
+    @Override
+    public void saveEmployee(Pid pid) {
+        persist(pid);
+    }
+
+    /**
+     * missing javadoc
+     *
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Pid> findAllPids() {
+        Criteria criteria = createEntityCriteria();
+        return (List<Pid>) criteria.list();
+    }
+
+    /**
+     * missing javadoc; currently incomplete
+     *
+     * @param regex
+     * @return
+     */
+    @Override
+    public Pid findPidByRegex(String regex) {
+        Criteria criteria = createEntityCriteria();
+        criteria.add(Restrictions.eq("ssn", regex));
+        return (Pid) criteria.uniqueResult();
+    }
+
+}
