fa6b069adbb6925d414236773b6f308e8f6d5cd3
Khyzad <lljruffin@gmail.com>
2016-02-28 14:27:45 -1000

    Create class Setting
    As with Pid, Setting is a POJO that will act as a superclass
    for the two types of settings UsedSetting and DefaultSetting

---
+++ Minter/src/main/java/com/hida/model/Setting.java
@@ -0,0 +1,133 @@
+package com.hida.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.hibernate.annotations.Type;
+
+/**
+ * missing javadoc
+ *
+ * @author lruffin
+ */
+@MappedSuperclass
+public class Setting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @Size(min = 0, max = 10)
+    @Column(name = "PREFIX", nullable = false)
+    @Type(type = "java.lang.String")
+    private String Prefix;
+
+    @Column(name = "TOKENTYPE")
+    @Type(type = "com.hida.model.TokenType")
+    private TokenType TokenType;
+
+    @Column(name = "CHARMAP")
+    @Type(type = "java.lang.String")
+    private String CharMap;
+
+    @Type(type = "short")
+    @Column(name = "ROOTLENGTH")
+    private int RootLength;
+
+    @NotNull
+    @Column(name = "SANSVOWELS")
+    @Type(type = "boolean")
+    private boolean SansVowels;
+
+    /**
+     * Copy constructor.
+     *
+     * @param s the CachedSetting to copy
+     */
+    public Setting(Setting s) {
+        Prefix = s.getPrefix();
+        TokenType = s.getTokenType();
+        CharMap = s.getCharMap();
+        RootLength = s.getRootLength();
+        SansVowels = s.isSansVowels();
+    }
+
+    /**
+     * Constructor; missing javadoc
+     *
+     * @param Prefix
+     * @param TokenType
+     * @param CharMap
+     * @param RootLength
+     * @param SansVowels
+     */
+    public Setting(String Prefix, TokenType TokenType, String CharMap, int RootLength,
+            boolean SansVowels) {
+        this.Prefix = Prefix;
+        this.TokenType = TokenType;
+        this.CharMap = CharMap;
+        this.RootLength = RootLength;
+        this.SansVowels = SansVowels;
+    }
+
+    /**
+     * Default constructor.
+     */
+    public Setting() {
+    }
+
+    // Typical getters and setters    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPrefix() {
+        return Prefix;
+    }
+
+    public void setPrefix(String Prefix) {
+        this.Prefix = Prefix;
+    }
+
+    public TokenType getTokenType() {
+        return TokenType;
+    }
+
+    public void setTokenType(TokenType TokenType) {
+        this.TokenType = TokenType;
+    }
+
+    public String getCharMap() {
+        return CharMap;
+    }
+
+    public void setCharMap(String CharMap) {
+        this.CharMap = CharMap;
+    }
+
+    public int getRootLength() {
+        return RootLength;
+    }
+
+    public void setRootLength(int RootLength) {
+        this.RootLength = RootLength;
+    }
+
+    public boolean isSansVowels() {
+        return SansVowels;
+    }
+
+    public void setSansVowels(boolean SansVowels) {
+        this.SansVowels = SansVowels;
+    }
+
+}
