278b53e55bbc0c997779e1b1d000957faf45ba5a
Khyzad <lljruffin@gmail.com>
2016-03-12 00:20:48 -1000

    Remove MessageSource
    Remove the imports and method associated with MessageSource and
    ResourceBundler.

--- Minter/src/main/java/com/hida/configuration/AppConfig.java
+++ Minter/src/main/java/com/hida/configuration/AppConfig.java
@@ -1,21 +1,18 @@
 package com.hida.configuration;
 
- 
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
- 
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.hida")
 public class AppConfig {
-     
+
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -24,11 +21,5 @@ public class AppConfig {
         viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
-     
-    @Bean
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
-        return messageSource;
-    }
+    
 }
