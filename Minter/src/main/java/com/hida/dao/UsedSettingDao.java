6a8b4fcfb0fdb86d04c01616ef9baa49661e5bc3
Khyzad <lljruffin@gmail.com>
2016-03-12 00:32:44 -1000

    Replace Setting import for UsedSetting import
    In addition to replacing the Setting import, all instances of
    Setting are replaced with UsedSetting. Spacing was also added.

--- Minter/src/main/java/com/hida/dao/UsedSettingDao.java
+++ Minter/src/main/java/com/hida/dao/UsedSettingDao.java
@@ -1,18 +1,24 @@
 package com.hida.dao;
 
-import com.hida.model.Setting;
+
 import com.hida.model.TokenType;
+import com.hida.model.UsedSetting;
 import java.util.List;
 
 /**
  *
  * @author lruffin
  */
-public interface SettingDao {
-    public void save(Setting setting);    
-    public void deleteSetting(Setting setting);
-    public List<Setting> findAllSettings();
-    public Setting findSettingById(int id);
-    public List<Setting> findSetting(String Prefix, TokenType TokenType, String CharMap, int RootLength,
-            boolean SansVowels);
+public interface UsedSettingDao {
+    public void save(UsedSetting setting);    
+    public void deleteSetting(UsedSetting setting);
+    
+    public UsedSetting findUsedSettingById(int id);
+    
+    public List<UsedSetting> findAllUsedSettings();    
+    public List<UsedSetting> findUsedSetting(String Prefix, TokenType TokenType, String CharMap,
+            int RootLength, boolean SansVowels);
+    
+    
+   
 }
