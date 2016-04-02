60d865c5cd1ab192dac175a42ddf5f5aa2559b35
Khyzad <lljruffin@gmail.com>
2016-04-02 13:24:08 -1000

    Add HibernateTestConfiguration
    This class sets up Hibernate so that the data layer can be tested. An
    in-memory database is explicitly used for this purpose.

--- Minter/src/test/java/com/hida/configuration/HibernateTestConfiguration.java
+++ Minter/src/test/java/com/hida/configuration/HibernateTestConfiguration.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.hida.configuration;
+
+import java.util.Properties;
+import javax.sql.DataSource; 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  *
  * @author lruffin
  */
+@Configuration
+@EnableTransactionManagement
+@ComponentScan({ "com.hida.dao" })
 public class HibernateTestConfiguration {
-    
+    @Autowired
+    private Environment environment;
+ 
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(new String[] { "com.hida.model" });
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+ 
+    @Bean(name = "dataSource")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.hsql.Driver");
+        dataSource.setUrl("jdbc:hsql:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+        dataSource.setUsername("");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+ 
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        return properties;
+    }
+ 
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory s) {
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(s);
+        return txManager;
+    }
 }
