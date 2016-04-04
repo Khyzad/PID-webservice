d95a28540b9f3de97f28f2d54bd522238f9c6b72
Khyzad <lljruffin@gmail.com>
2016-04-03 14:11:41 -1000

    Update saveTest in UsedSettingDaoImplTest

--- Minter/src/test/java/com/hida/dao/UsedSettingDaoImplTest.java
+++ Minter/src/test/java/com/hida/dao/UsedSettingDaoImplTest.java
@@ -1,7 +1,8 @@
-
 package com.hida.dao;
 
 import com.hida.configuration.HsqlDataTypeFactory;
+import com.hida.model.TokenType;
+import com.hida.model.UsedSetting;
 import java.sql.Connection;
 import org.dbunit.database.DatabaseConfig;
 import org.dbunit.database.DatabaseConnection;
@@ -18,43 +19,54 @@ import org.testng.annotations.Test;
  *
  * @author lruffin
  */
-public class UsedSettingDaoImplTest extends EntityDaoImplTest{
+public class UsedSettingDaoImplTest extends EntityDaoImplTest {
 
     @Autowired
     UsedSettingDao UsedSettingDao;
-    
+
     @Override
     protected IDataSet getDataSet() throws Exception {
         IDataSet dataSet = new FlatXmlDataSet(this.getClass().getClassLoader().
                 getResourceAsStream("UsedSetting.xml"));
-        
-        return dataSet; 
-    }       
-    
+
+        return dataSet;
+    }
+
     @Test
-    public void save(){
-        Assert.fail("unimplemented");
-    } 
-    
+    public void save() {        
+        UsedSettingDao.save(getSampleUsedSetting());
+        Assert.assertEquals(UsedSettingDao.findAllUsedSettings().size(), 3);
+    }
+
     @Test
-    public void deleteSetting(){
+    public void deleteSetting() {
         Assert.fail("unimplemented");
     }
-    
+
     @Test
-    public void findUsedSettingById(){
+    public void findUsedSettingById() {
         Assert.fail("unimplemented");
     }
-    
+
     @Test
-    public void findAllUsedSettings(){
-        Assert.fail("unimplemented");
-    } 
-    
-    @Test
-    public void findUsedSetting(){
+    public void findAllUsedSettings() {
         Assert.fail("unimplemented");
     }
-    
-  
+
+    @Test
+    public void findUsedSetting() {
+        Assert.fail("unimplemented");
+    }
+
+    private UsedSetting getSampleUsedSetting() {
+        UsedSetting setting = new UsedSetting("",
+                TokenType.DIGIT,
+                "d",
+                1,
+                true,
+                1);
+
+        return setting;
+    }
+
 }
