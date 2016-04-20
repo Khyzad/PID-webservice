432c6514afc6e474baa8978929e87d3e0f188d4a
Khyzad <lljruffin@gmail.com>
2016-04-19 21:23:38 -1000

    Add javadocs to DefaultSettingDao

--- Minter/src/main/java/com/hida/dao/DefaultSettingDao.java
+++ Minter/src/main/java/com/hida/dao/DefaultSettingDao.java
@@ -1,17 +1,17 @@
 package com.hida.dao;
 
-
 import com.hida.model.DefaultSetting;
 
-
 /**
+ * This class is used to define the possible operations that Hibernate can
+ * perform on DefaultSetting objects
  *
  * @author lruffin
  */
 public interface DefaultSettingDao {
-    public void save(DefaultSetting setting);        
-    
+
+    public void save(DefaultSetting setting);
+
     public DefaultSetting getDefaultSetting();
-    
-   
+
 }
