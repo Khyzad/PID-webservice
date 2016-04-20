ea09292df2b4a2cd756681c35d86a7cc02a8992e
Khyzad <lljruffin@gmail.com>
2016-04-19 21:38:55 -1000

    Add javadocs to UsedSettingDao

--- Minter/src/main/java/com/hida/dao/UsedSettingDao.java
+++ Minter/src/main/java/com/hida/dao/UsedSettingDao.java
@@ -1,21 +1,22 @@
 package com.hida.dao;
 
-
 import com.hida.model.UsedSetting;
 import java.util.List;
 
 /**
+ * This class is used to define the possible operations that Hibernate can
+ * perform on UsedSettingDao objects
  *
  * @author lruffin
  */
 public interface UsedSettingDao {
-    public void save(UsedSetting setting);    
-    
+
+    public void save(UsedSetting setting);
+
     public UsedSetting findUsedSettingById(int id);
-    
-    public List<UsedSetting> findAllUsedSettings();    
+
+    public List<UsedSetting> findAllUsedSettings();
+
     public UsedSetting findUsedSetting(UsedSetting setting);
-    
-    
-   
+
 }
