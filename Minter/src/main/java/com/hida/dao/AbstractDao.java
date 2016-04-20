4b9299305517d5a7382128c843cbea2197c0e452
Khyzad <lljruffin@gmail.com>
2016-04-19 21:07:59 -1000

    Add javadocs to AbstractDao

--- Minter/src/main/java/com/hida/dao/AbstractDao.java
+++ Minter/src/main/java/com/hida/dao/AbstractDao.java
@@ -2,52 +2,51 @@ package com.hida.dao;
 
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
-
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
- * missing javadocs
+ * AbstractDao acts as base implementation for all Dao classes and provides
+ * wrapper methods for all Hibernate operations
  *
  * @author lruffin
- * @param <PK>
- * @param <T>
+ * @param <PK> Persistent Key of the calling POJO
+ * @param <T> The POJO
  */
 public abstract class AbstractDao<PK extends Serializable, T> {
 
     /**
-     *      
+     * The class T
      */
     private final Class<T> persistentClass;
 
     /**
-     * 
+     * Constructor
      */
     @SuppressWarnings("unchecked")
     public AbstractDao() {
         this.persistentClass = (Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[1];
     }
 
-    /**
-     * 
-     */
     @Autowired
     private SessionFactory sessionFactory;
 
     /**
-     * 
-     * @return 
+     * Returns the current session associated with the current transaction
+     *
+     * @return the current session
      */
     protected Session getSession() {
         return sessionFactory.getCurrentSession();
     }
 
     /**
-     * 
-     * @param key
-     * @return 
+     * Retrieves a persisted object using a persistent key
+     *
+     * @param key The persistent key
+     * @return persisted object
      */
     @SuppressWarnings("unchecked")
     public T getByKey(PK key) {
@@ -55,25 +54,28 @@ public abstract class AbstractDao<PK extends Serializable, T> {
     }
 
     /**
-     * 
-     * @param entity 
+     * Persists an object
+     *
+     * @param entity Object to be saved
      */
-        
     public void persist(T entity) {
         getSession().persist(entity);
     }
 
     /**
-     * 
-     * @param entity 
+     * Removes an object from persistence
+     *
+     * @param entity Object to be removed
      */
     public void delete(T entity) {
         getSession().delete(entity);
     }
 
     /**
-     * 
-     * @return 
+     * Creates and returns a Criteria object that can be used to filter
+     * persisted objects
+     *
+     * @return Criteria
      */
     protected Criteria createEntityCriteria() {
         return getSession().createCriteria(persistentClass);
