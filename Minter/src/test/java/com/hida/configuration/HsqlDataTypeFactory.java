a10ed2ef56d67a94d1ad7ad7da0ea65e896866cf
Khyzad <lljruffin@gmail.com>
2016-04-20 02:05:07 -1000

    Add javadocs to HsqlDataTypeFactory

--- Minter/src/test/java/com/hida/configuration/HsqlDataTypeFactory.java
+++ Minter/src/test/java/com/hida/configuration/HsqlDataTypeFactory.java
@@ -1,4 +1,3 @@
-
 package com.hida.configuration;
 
 import org.dbunit.dataset.datatype.DataType;
@@ -7,17 +6,20 @@ import org.dbunit.dataset.datatype.DefaultDataTypeFactory;
 import org.hsqldb.types.Types;
 
 /**
+ * There is currently a bug with HSQL where booleans created by HSQL cannot be
+ * read by Hibernate. This class intercepts the creation of entities and changes
+ * the HSQL-accepted booleans to Hibernate-accepted booleans.
  *
  * @author lruffin
  */
 public class HsqlDataTypeFactory extends DefaultDataTypeFactory {
 
-	@Override
-	public DataType createDataType(int sqlType, String sqlTypeName) throws DataTypeException {
-		if (sqlType == Types.BOOLEAN) {
-			return DataType.BOOLEAN;
-		}
-		
-		return super.createDataType(sqlType, sqlTypeName);
-	}
+    @Override
+    public DataType createDataType(int sqlType, String sqlTypeName) throws DataTypeException {
+        if (sqlType == Types.BOOLEAN) {
+            return DataType.BOOLEAN;
+        }
+
+        return super.createDataType(sqlType, sqlTypeName);
+    }
 }
