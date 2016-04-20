bbfe7af319a5d3f0d642b2b43c7e7d45d79c7728
Khyzad <lljruffin@gmail.com>
2016-04-20 01:42:01 -1000

    Add javadocs to MinterService

--- Minter/src/main/java/com/hida/service/MinterService.java
+++ Minter/src/main/java/com/hida/service/MinterService.java
@@ -5,13 +5,17 @@ import com.hida.model.Pid;
 import java.util.Set;
 
 /**
+ * This class is used to define the possible operations that Hibernate can
+ * perform on MinterService objects
  *
  * @author lruffin
  */
-public interface MinterService {                                    
+public interface MinterService {
+
     public Set<Pid> mint(long amount, DefaultSetting setting);
-    
+
     public DefaultSetting getCurrentSetting();
+
     public void updateCurrentSetting(DefaultSetting newSetting);
-    
+
 }
