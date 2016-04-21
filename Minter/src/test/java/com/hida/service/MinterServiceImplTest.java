e71396c3a1ee7857784475f2d23c634454709179
Khyzad <lljruffin@gmail.com>
2016-04-20 19:45:15 -1000

    Clean and add javadocs to MinterServiceImplTest
    In an effort to create cleaner code, the following was done:
    - remove unused imports
    - remove unnecessary spaces
    - updated old javadocs where necessary
    - simplified certain tests

--- Minter/src/test/java/com/hida/service/MinterServiceImplTest.java
+++ Minter/src/test/java/com/hida/service/MinterServiceImplTest.java
@@ -3,7 +3,6 @@ package com.hida.service;
 import com.hida.dao.DefaultSettingDao;
 import com.hida.dao.PidDao;
 import com.hida.dao.UsedSettingDao;
-import com.hida.model.BadParameterException;
 import com.hida.model.DefaultSetting;
 import com.hida.model.NotEnoughPermutationsException;
 import com.hida.model.Pid;
@@ -15,7 +14,6 @@ import java.util.TreeSet;
 import org.mockito.Mock;
 import org.mockito.InjectMocks;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doNothing;
@@ -23,13 +21,12 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 import org.testng.Assert;
-
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
- * missing javadoc
+ * This class tests the functionality of MinterServiceImpl using Mockito.
  *
  * @author lruffin
  */
@@ -52,20 +49,21 @@ public class MinterServiceImplTest {
     Set<Pid> PidSet = new TreeSet<>();
 
     /**
-     * missing javadoc
+     * Sets up Mockito
      *
      * @throws Exception
      */
     @BeforeClass
     public void setUpClass() throws Exception {
         MockitoAnnotations.initMocks(this);
-        initializeDefaultSetting();
+        initializeDefaultSettingList();
         initializePidSet();
     }
 
     /**
+     * Test the various mint settings (auto/random and random/sequential)
      *
-     * @return
+     * @return An array of values
      */
     @DataProvider(name = "mintSettings")
     public Object[][] mintSettings() {
@@ -78,147 +76,180 @@ public class MinterServiceImplTest {
     }
 
     /**
-     * missing javadoc
+     * Tests the MinterServiceImpl by assuming that the settings aren't
+     * currently stored in the database
      *
-     * @param isRandom
-     * @param isAuto
+     * @param isRandom Determines if the PIDs are created randomly or
+     * sequentially
+     * @param isAuto Determines which generator, either Auto or Custom, will be
+     * used
      */
     @Test(dataProvider = "mintSettings")
     public void testMintWithNewUsedSetting(boolean isRandom, boolean isAuto) {
+        // retrieve a sample DefaultSetting entity
         DefaultSetting defaultSetting = DefaultSettingList.get(1);
         defaultSetting.setAuto(isAuto);
         defaultSetting.setRandom(isRandom);
 
+        // assume that any Pids created aren't already persisted and pretend to persist them
         when(PidDao.findByName(any(String.class))).thenReturn(null);
         doNothing().when(PidDao).savePid(any(Pid.class));
-        when(UsedSettingDao.findUsedSetting(any(UsedSetting.class))).thenReturn(null); 
+
+        // assume the UsedSetting isn't persisted and pretend to persist it
+        when(UsedSettingDao.findUsedSetting(any(UsedSetting.class))).thenReturn(null);
         doNothing().when(UsedSettingDao).save(any(UsedSetting.class));
 
+        // check to see if all the Pids were created
         Set<Pid> testSet = MinterServiceImpl.mint(10, defaultSetting);
         boolean containsAll = testSet.containsAll(PidSet);
         Assert.assertEquals(containsAll, true);
     }
 
     /**
-     * missing javadoc
+     * Tests the MinterServiceImpl under the scenario where UsedSetting entity
+     * with matching parameters already exist.
      *
-     * @param isAuto
-     * @param isRandom
+     * @param isRandom Determines if the PIDs are created randomly or
+     * sequentially
+     * @param isAuto Determines which generator, either Auto or Custom, will be
+     * used
      */
     @Test(dataProvider = "mintSettings")
     public void testMintWithOldUsedSetting(boolean isAuto, boolean isRandom) {
+        // retrieve a sample DefaultSetting entity
         DefaultSetting defaultSetting = DefaultSettingList.get(1);
         defaultSetting.setAuto(isAuto);
         defaultSetting.setRandom(isRandom);
+        
+        // get a sample UsedSetting entity
+        UsedSetting usedSetting = getSampleUsedSetting();
 
+        // assume that any Pids created aren't already persisted and pretend to persist them
         when(PidDao.findByName(any(String.class))).thenReturn(null);
         doNothing().when(PidDao).savePid(any(Pid.class));
-        when(UsedSettingDao.findUsedSetting(any(UsedSetting.class))).thenReturn(null); 
-        
-        doNothing().when(UsedSettingDao).save(any(UsedSetting.class));
+
+        // assume the UsedSetting isn't persisted and pretend to persist it
+        when(UsedSettingDao.findUsedSetting(any(UsedSetting.class))).thenReturn(usedSetting);
         doNothing().when(UsedSettingDao).save(any(UsedSetting.class));
 
+        // check to see if all the Pids were created
         Set<Pid> testSet = MinterServiceImpl.mint(5, defaultSetting);
         boolean containsAll = PidSet.containsAll(testSet);
         Assert.assertEquals(containsAll, true);
     }
 
     /**
-     * missing javadoc
+     * Tests the MinterServiceImpl to ensure that a
+     * NotEnoughPermutationsException is thrown whenever the amount retrieved
+     * from FindUsedSetting is less than the requested amount.
      *
-     * @param isAuto
-     * @param isRandom
+     * @param isRandom Determines if the PIDs are created randomly or
+     * sequentially
+     * @param isAuto Determines which generator, either Auto or Custom, will be
+     * used
      */
     @Test(expectedExceptions = NotEnoughPermutationsException.class, dataProvider = "mintSettings")
     public void testMintNotEnoughPermutationsExceptionInFindUsedSetting(
             boolean isAuto, boolean isRandom) {
+        // retrieve a sample DefaultSetting entity
         DefaultSetting defaultSetting = DefaultSettingList.get(1);
         defaultSetting.setAuto(isAuto);
         defaultSetting.setRandom(isRandom);
 
-        UsedSetting usedSetting = new UsedSetting("", // prefix
-                TokenType.DIGIT, // tokentype
-                "d", // charmap
-                1, // rootlength
-                true, //sans vowels
-                5); // amount
+        // get a sample UsedSetting entity
+        UsedSetting usedSetting = getSampleUsedSetting();
 
+        // assume that any Pids created aren't already persisted and pretend to persist them
         when(PidDao.findByName(any(String.class))).thenReturn(null);
         doNothing().when(PidDao).savePid(any(Pid.class));
+
+        // pretend to find and retrieve variable usedSetting
         when(UsedSettingDao.findUsedSetting(any(UsedSetting.class))).thenReturn(usedSetting);
         when(UsedSettingDao.findUsedSettingById(anyInt())).thenReturn(usedSetting);
-        doNothing().when(UsedSettingDao).save(any(UsedSetting.class));
 
+        // try to mint an amount greater than what is available
         Set<Pid> testSet = MinterServiceImpl.mint(6, defaultSetting);
     }
 
     /**
-     * missing javadoc
+     * Tests the MinterServiceImpl to ensure that a
+     * NotEnoughPermutationsException is thrown whenever the requested amount of
+     * Pids to mint exceeds the possible number of permutations.
      *
-     * @param isAuto
-     * @param isRandom
+     * @param isRandom Determines if the PIDs are created randomly or
+     * sequentially
+     * @param isAuto Determines which generator, either Auto or Custom, will be
+     * used
      */
     @Test(expectedExceptions = NotEnoughPermutationsException.class, dataProvider = "mintSettings")
     public void testMintNotEnoughPermutationsExceptionInCalculatePermutations(
             boolean isAuto, boolean isRandom) {
+        // retrieve a sample DefaultSetting entity
         DefaultSetting defaultSetting = DefaultSettingList.get(1);
         defaultSetting.setAuto(isAuto);
         defaultSetting.setRandom(isRandom);
-        
-        UsedSetting usedSetting = new UsedSetting("", // prefix
-                TokenType.DIGIT, // tokentype
-                "d", // charmap
-                1, // rootlength
-                true, //sans vowels
-                0); // amount
 
+        // assume that any Pids created aren't already persisted and pretend to persist them
         when(PidDao.findByName(any(String.class))).thenReturn(null);
         doNothing().when(PidDao).savePid(any(Pid.class));
-        when(UsedSettingDao.findUsedSetting(usedSetting)).thenReturn(usedSetting);
+
+        // assume that UsedSetting entity with the relevant parameters does not exist
+        when(UsedSettingDao.findUsedSetting(any(UsedSetting.class))).thenReturn(null);
         doNothing().when(UsedSettingDao).save(any(UsedSetting.class));
 
+        // try to mint an amount greater than what is possible
         Set<Pid> testSet = MinterServiceImpl.mint(11, defaultSetting);
     }
 
     /**
-     * missing javadoc
+     * Tests the MinterServiceImpl to ensure that a
+     * NotEnoughPermutationsException is thrown whenever it is no longer
+     * possible to 'roll' Pids. This is important because there may be different
+     * settings that may have created Pids that could match the fields of the
+     * currently used setting.
      *
-     * @param isAuto
-     * @param isRandom
+     * @param isRandom Determines if the PIDs are created randomly or
+     * sequentially
+     * @param isAuto Determines which generator, either Auto or Custom, will be
+     * used
      */
     @Test(expectedExceptions = NotEnoughPermutationsException.class, dataProvider = "mintSettings")
     public void testMintNotEnoughPermutationExceptionInRollId(boolean isAuto, boolean isRandom) {
+        // retrieve a sample DefaultSetting entity
         DefaultSetting defaultSetting = DefaultSettingList.get(1);
         defaultSetting.setAuto(isAuto);
         defaultSetting.setRandom(isRandom);
-        
-        UsedSetting usedSetting = new UsedSetting("", // prefix
-                TokenType.DIGIT, // tokentype
-                "d", // charmap
-                1, // rootlength
-                true, //sans vowels
-                0); // amount
 
+        // pretend any Pid with the name "0" is the only Pid that exists
+        when(PidDao.findByName(any(String.class))).thenReturn(null);
         when(PidDao.findByName("0")).thenReturn(new TestPid(0));
         doNothing().when(PidDao).savePid(any(Pid.class));
-        when(UsedSettingDao.findUsedSetting(usedSetting)).thenReturn(usedSetting);
+
+        // assume that UsedSetting entity with the relevant parameters does not exist
+        when(UsedSettingDao.findUsedSetting(any(UsedSetting.class))).thenReturn(null);
         doNothing().when(UsedSettingDao).save(any(UsedSetting.class));
 
+        // try to mint an amount greater than what is possible
         Set<Pid> testSet = MinterServiceImpl.mint(10, defaultSetting);
     }
 
     /**
-     * missing javadoc
+     * Test in MinterServiceImpl that ensures that the CurrentSetting is sought
+     * after.
      */
     @Test
     public void testGetCurrentSettingWithExistingDefaultSetting() {
         DefaultSetting defaultSetting = DefaultSettingList.get(0);
         when(DefaultSettingDao.getDefaultSetting()).thenReturn(defaultSetting);
-        Assert.assertEquals(MinterServiceImpl.getCurrentSetting(), defaultSetting);
+
+        MinterServiceImpl.getCurrentSetting();
+        verify(DefaultSettingDao, atLeastOnce()).getDefaultSetting();
     }
 
     /**
-     * missing javadoc
+     * Test in MinterServiceImpl that ensures that the CurrentSetting is sought
+     * after and if it does not exist, a new DefaultSetting is created and
+     * saved.
      */
     @Test
     public void testGetCurrentSettingWithoutExistingDefaultSetting() {
@@ -237,7 +268,8 @@ public class MinterServiceImplTest {
     }
 
     /**
-     * missing javadoc
+     * Test in MinterServiceImpl that checks if CurrentSetting in
+     * MinterServiceImpl is being properly updated.
      */
     @Test
     public void testUpdateCurrentSetting() {
@@ -249,9 +281,9 @@ public class MinterServiceImplTest {
     }
 
     /**
-     * missing javadoc
+     * Create a list of sample DefaultSettingList
      */
-    private void initializeDefaultSetting() {
+    private void initializeDefaultSettingList() {
         DefaultSetting defaultSetting1 = new DefaultSetting("", // prepend
                 "", // prefix
                 TokenType.DIGIT, // token type
@@ -276,7 +308,7 @@ public class MinterServiceImplTest {
     }
 
     /**
-     * missing javadoc
+     * Create a sample set of Pid
      */
     private void initializePidSet() {
         for (int i = 0; i < 10; i++) {
@@ -285,7 +317,21 @@ public class MinterServiceImplTest {
     }
 
     /**
-     * missing javadoc
+     * Return a sample UsedSetting
+     *
+     * @return
+     */
+    private UsedSetting getSampleUsedSetting() {
+        return new UsedSetting("", // prefix
+                TokenType.DIGIT, // tokentype
+                "d", // charmap
+                1, // rootlength
+                true, //sans vowels
+                5); // amount
+    }
+
+    /**
+     * A test class used to create Pid
      */
     private class TestPid extends Pid {
 
@@ -297,6 +343,6 @@ public class MinterServiceImplTest {
         @Override
         public boolean incrementId() {
             throw new UnsupportedOperationException("Not supported yet.");
-        }       
+        }
     }
 }
