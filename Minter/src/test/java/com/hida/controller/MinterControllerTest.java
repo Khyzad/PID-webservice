6d51607092a37a9fd3d9a8b46137867e5c5d1880
Khyzad <lljruffin@gmail.com>
2016-04-21 12:14:54 -1000

    Clean and add javadocs to MinterServiceImplTest
    In an effort to create cleaner code, the following was done:
    - remove unused imports
    - remove unnecessary spaces
    - updated old javadocs where necessary
    - simplified certain tests
    
    missing changes:
    - missing javadocs

--- Minter/src/test/java/com/hida/controller/MinterControllerTest.java
+++ Minter/src/test/java/com/hida/controller/MinterControllerTest.java
@@ -33,6 +33,7 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.testng.annotations.DataProvider;
 
 /**
+ * Class that tests MinterController
  *
  * @author lruffin
  */
@@ -57,6 +58,11 @@ public class MinterControllerTest {
         MockitoAnnotations.initMocks(this);
     }
 
+    /**
+     * Returns a data set. Each array contains attributes found in setting.jsp
+     *
+     * @return A data set
+     */
     @DataProvider(name = "request parameters")
     private Object[][] requestParameters() {
         return new Object[][]{
@@ -66,11 +72,15 @@ public class MinterControllerTest {
             {PREPEND, "xyz", "false", "false", null, "1", null, "true", null, "mmm"},
             {PREPEND, "xyz", "false", "false", null, "1", null, "true", "true", "mmm"},
             {PREPEND, "xyz", "false", "false", null, "1", null, null, "true", "mmm"},
-            {PREPEND, "xyz", "false", "false", null, "1", "true", null, null, "mmm"},
-            
-        };
+            {PREPEND, "xyz", "false", "false", null, "1", "true", null, null, "mmm"},};
     }
 
+    /**
+     * Returns a data set. Each array contains fields that can be exclusively
+     * held by a DefaultSetting.
+     *
+     * @return A dataset
+     */
     @DataProvider(name = "parameters")
     private Object[][] parameters() {
         return new Object[][]{
@@ -95,22 +105,54 @@ public class MinterControllerTest {
             {PREPEND, "abc", TokenType.DIGIT, "e", 1, false, true, false},};
     }
 
+    /**
+     * Returns a dataset that holds colloquially accepted values for booleans
+     * that automatically parses as false by the Boolean static methods.
+     *
+     * @return A data set
+     */
     @DataProvider(name = "bad booleans")
     private Object[][] badBooleans() {
         return new Object[][]{
             {"auto", "t"},
-            {"random", "t"},
-            {"sansVowels", "t"},};
+            {"random", "f"},
+            {"sansVowels", "yes"},};
     }
 
+    /**
+     * Tests data entry in the administration panel found at setting.jsp.
+     *
+     * The following parameters are parameters that are contained in the request
+     * object
+     *
+     * @param prepend Primarily used to turn a Pid into a PURL
+     * @param idprefix A sequence of characters that appear in the beginning of
+     * PIDs
+     * @param mintType Determines which IdGenerator, auto or random, to use
+     * @param mintOrder Determines the order, randomly or sequentially, the Pids
+     * are minted
+     * @param vowels Boolean that determines whether or not the the Pids should
+     * contain vowels
+     * @param idLength Designates the length of the id's root
+     * @param digits Boolean that determines whether or not the Pids will
+     * contain numbers
+     * @param lowercase Boolean that determines whether or not the Pids will
+     * contain lowercase
+     * @param uppercase Boolean that determines whether or not the Pids will
+     * contain uppercase
+     * @param charMapping A sequence of characters used to configure PIDs
+     * @throws Exception
+     */
     @Test(dataProvider = "request parameters")
     public void testDisplayIndex(String prepend, String idprefix, String mintType,
             String mintOrder, String vowels, String idLength, String digits,
             String lowercase, String uppercase, String charMapping) throws Exception {
+        // create a DefaultSetting object and create mock request and response object
         DefaultSetting originalSetting = getSampleDefaultSetting();
         MockHttpServletRequest request = new MockHttpServletRequest();
-        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);                
+        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
 
+        // assign values to the mock request object
         request.setParameter("prepend", prepend);
         request.setParameter("idprefix", idprefix);
         request.setParameter("mintType", mintType);
@@ -120,21 +162,32 @@ public class MinterControllerTest {
         request.setParameter("digits", digits);
         request.setParameter("lowercase", lowercase);
         request.setParameter("uppercase", uppercase);
-        request.setParameter("charmapping", charMapping);      
-        
+        request.setParameter("charmapping", charMapping);
+
+        // return originalSetting whenever CurrentSetting is called
         when(MinterServiceDao.getCurrentSetting()).thenReturn(originalSetting);
         doNothing().when(MinterServiceDao).updateCurrentSetting(originalSetting);
 
         Assert.assertEquals("redirect:", Controller.handleForm(request, response));
     }
 
+    /**
+     * Tests the /mint endpoint by submitting parameters through it.
+     *
+     * @throws Exception
+     */
     @Test
-    public void testRequestedParameters() throws Exception {
-        //ModelMap.clear();
+    public void testMintRequestedParameters() throws Exception {
+        // create a map to hold parameters
         Map<String, String> map = getSampleMap();
+
+        // create DefaultSetting object to represent the original object
         DefaultSetting originalSetting = getSampleDefaultSetting();
+
+        // create DefaultSetting object to represent the changes
         DefaultSetting tempSetting = getSampleDefaultSetting();
 
+        // change the values of tempSetting to anticipate the changes done overrideSetting methods
         tempSetting.setPrepend(map.get("prepend"));
         tempSetting.setPrefix(map.get("prefix"));
         tempSetting.setRootLength(Integer.parseInt(map.get("rootLength")));
@@ -144,17 +197,21 @@ public class MinterControllerTest {
         tempSetting.setRandom(Boolean.getBoolean(map.get("random")));
         tempSetting.setSansVowels(Boolean.getBoolean(map.get("sansVowels")));
 
+        // return originalSetting when getCurrentSetting is called and a sample Pid set 
         when(MinterServiceDao.getCurrentSetting()).thenReturn(originalSetting);
         when(MinterServiceDao.mint(anyInt(), any(DefaultSetting.class))).
                 thenReturn(getSampleSet(tempSetting));
 
+        // check to see if the correct jsp page is returned
         String jspName = Controller.printPids(AMOUNT, ModelMap, map);
         Assert.assertEquals("mint", jspName);
 
+        // create Json objects to extract Json array
         String message = (String) ModelMap.get("message");
         Logger.debug(message);
         JSONArray testJsonArray = new JSONArray(message);
 
+        // test the pid and ensure that they match the used setting
         for (int i = 0; i < AMOUNT; i++) {
             JSONObject object = testJsonArray.getJSONObject(i);
             int id = object.getInt("id");
@@ -164,6 +221,22 @@ public class MinterControllerTest {
         }
     }
 
+    /**
+     * Tests to see if the MinterController will call persisted settings
+     *
+     * @param prepend Primarily used to turn a Pid into a PURL
+     * @param prefix A sequence of characters that appear in the beginning of
+     * PIDs
+     * @param tokenType An enum used to configure PIDS
+     * @param charMap A sequence of characters used to configure PIDs
+     * @param rootLength Designates the length of the id's root
+     * @param isAuto Determines which generator, either Auto or Custom, will be
+     * used
+     * @param isRandom Determines if the PIDs are created randomly or
+     * sequentially
+     * @param sansVowel Dictates whether or not vowels are allowed
+     * @throws Exception
+     */
     @Test(dataProvider = "parameters")
     public void testMintPersistedParameters(String prepend, String prefix, TokenType tokenType,
             String charMap, int rootLength, boolean isAuto, boolean isRandom, boolean sansVowel)
@@ -176,13 +249,16 @@ public class MinterControllerTest {
         when(MinterServiceDao.mint(anyInt(), any(DefaultSetting.class))).
                 thenReturn(getSampleSet(setting));
 
+        // check to see if the correct jsp page is returned
         String jspName = Controller.printPids(AMOUNT, ModelMap, new HashMap<String, String>());
         Assert.assertEquals("mint", jspName);
 
+        // create Json objects to extract Json array
         String message = (String) ModelMap.get("message");
         Logger.debug(message);
         JSONArray testJsonArray = new JSONArray(message);
 
+        // test the pid and ensure that they match the used setting
         for (int i = 0; i < testJsonArray.length(); i++) {
             JSONObject object = testJsonArray.getJSONObject(i);
             int id = object.getInt("id");
@@ -192,6 +268,14 @@ public class MinterControllerTest {
         }
     }
 
+    /**
+     * Tests to see if the MinterController will properly throw an error when a
+     * non-boolean is entered into the /mint endpoint
+     *
+     * @param key The boolean
+     * @param value The entered value of the boolean
+     * @throws Exception
+     */
     @Test(expectedExceptions = BadParameterException.class, dataProvider = "bad booleans")
     public void testBadParameterExceptionBoolean(String key, String value) throws Exception {
         Map<String, String> parameters = new HashMap<>();
@@ -203,6 +287,12 @@ public class MinterControllerTest {
         Controller.printPids(AMOUNT, ModelMap, parameters);
     }
 
+    /**
+     * Tests to see if MinterController will properly throw an error when an
+     * invalid tokenType is entered into the /mint endpoint
+     *
+     * @throws Exception
+     */
     @Test(expectedExceptions = BadParameterException.class)
     public void testBadParameterExceptionTokenType() throws Exception {
         Map<String, String> parameters = new HashMap<>();
@@ -214,6 +304,12 @@ public class MinterControllerTest {
         Controller.printPids(AMOUNT, ModelMap, parameters);
     }
 
+    /**
+     * Tests to see if MinterController will properly throw an error when an
+     * invalid charMap is entered into the /mint endpoint
+     *
+     * @throws Exception
+     */
     @Test(expectedExceptions = BadParameterException.class)
     public void testBadParameterExceptionCharMap() throws Exception {
         Map<String, String> parameters = new HashMap<>();
@@ -225,6 +321,12 @@ public class MinterControllerTest {
         Controller.printPids(AMOUNT, ModelMap, parameters);
     }
 
+    /**
+     * Tests to see if MinterController will properly throw an error when an
+     * invalid amount is entered into the /mint endpoint
+     *
+     * @throws Exception
+     */
     @Test(expectedExceptions = BadParameterException.class)
     public void testBadParameterExceptionAmount() throws Exception {
         Map<String, String> parameters = new HashMap<>();
@@ -234,7 +336,13 @@ public class MinterControllerTest {
         when(MinterServiceDao.getCurrentSetting()).thenReturn(setting);
         Controller.printPids(-1, ModelMap, parameters);
     }
-    
+
+    /**
+     * Tests to see if MinterController will properly throw an error when an
+     * invalid prefix is entered into the /mint endpoint
+     *
+     * @throws Exception
+     */
     @Test(expectedExceptions = BadParameterException.class)
     public void testBadParameterExceptionPrefix() throws Exception {
         Map<String, String> parameters = new HashMap<>();
@@ -246,6 +354,12 @@ public class MinterControllerTest {
         Controller.printPids(AMOUNT, ModelMap, parameters);
     }
 
+    /**
+     * Tests a name using to see if it matches the values provided by setting
+     *
+     * @param name
+     * @param setting
+     */
     private void testPid(String name, DefaultSetting setting) {
         testPidPrepend(name, setting);
         testPidPrefix(name, setting);
@@ -260,12 +374,24 @@ public class MinterControllerTest {
 
     }
 
+    /**
+     * Tests the name to see if it matches the provided prepend
+     *
+     * @param name
+     * @param setting
+     */
     private void testPidPrepend(String name, DefaultSetting setting) {
         String prepend = setting.getPrepend();
 
         Assert.assertTrue(name + " testing prepend", name.startsWith(prepend));
     }
 
+    /**
+     * Tests the name to see if it matches the provided prefix
+     *
+     * @param name
+     * @param setting
+     */
     private void testPidPrefix(String name, DefaultSetting setting) {
         String prepend = setting.getPrepend();
         String prefix = setting.getPrefix();
@@ -273,6 +399,12 @@ public class MinterControllerTest {
         Assert.assertTrue(name + " testing prepend", name.startsWith(prepend + prefix));
     }
 
+    /**
+     * Tests the name to see if it matches the provided token type
+     *
+     * @param name
+     * @param setting
+     */
     private void testPidTokenType(String name, DefaultSetting setting) {
         String prepend = setting.getPrepend();
         String prefix = setting.getPrefix();
@@ -285,6 +417,12 @@ public class MinterControllerTest {
         Assert.assertEquals(name + " testing tokenType", true, matchesToken);
     }
 
+    /**
+     * Tests the name to see if it matches the provided root length
+     *
+     * @param name
+     * @param setting
+     */
     private void testPidRootLength(String name, DefaultSetting setting) {
         String prepend = setting.getPrepend();
         String prefix = setting.getPrefix();
@@ -293,6 +431,12 @@ public class MinterControllerTest {
         Assert.assertEquals(name + " testing rootLength", name.length(), setting.getRootLength());
     }
 
+    /**
+     * Tests the name to see if it matches the provided char map
+     *
+     * @param name
+     * @param setting
+     */
     private void testPidCharMap(String name, DefaultSetting setting) {
         String prepend = setting.getPrepend();
         String prefix = setting.getPrefix();
@@ -396,6 +540,10 @@ public class MinterControllerTest {
         return regex;
     }
 
+    /**
+     * Returns a sample DefaultSetting object
+     * @return 
+     */
     private DefaultSetting getSampleDefaultSetting() {
         DefaultSetting setting = new DefaultSetting("", // prepend
                 "", // prefix
@@ -409,6 +557,11 @@ public class MinterControllerTest {
         return setting;
     }
 
+    /**
+     * Returns a sample set of Pids
+     * @param setting
+     * @return 
+     */
     private Set<Pid> getSampleSet(DefaultSetting setting) {
         IdGenerator generator;
         if (setting.isAuto()) {
@@ -435,6 +588,10 @@ public class MinterControllerTest {
         return set;
     }
 
+    /**
+     * Returns a sample map object
+     * @return 
+     */
     private Map<String, String> getSampleMap() {
         Map<String, String> map = new HashMap<>();
 
