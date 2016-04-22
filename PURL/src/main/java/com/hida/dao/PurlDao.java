1c34dcfdd078be4bdda4272decbdf64d1675acaf
Khyzad <lljruffin@gmail.com>
2016-04-22 00:48:40 -1000

    Add stub methods in Dao classes

--- PURL/src/main/java/com/hida/dao/PurlDao.java
+++ PURL/src/main/java/com/hida/dao/PurlDao.java
@@ -1,9 +1,16 @@
 package com.hida.dao;
 
+import com.hida.model.Purl;
+
 /**
  *
  * @author lruffin
  */
 public interface PurlDao {
-    
+
+    public Purl findByPurl(String purl);
+
+    public void save(Purl purl);
+
+    public void delete(Purl purl);
 }
--- PURL/src/main/java/com/hida/dao/PurlDaoImpl.java
+++ PURL/src/main/java/com/hida/dao/PurlDaoImpl.java
@@ -1,11 +1,28 @@
 package com.hida.dao;
 
 import com.hida.model.Purl;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author lruffin
  */
+@Repository("defaultSettingDao")
 public class PurlDaoImpl extends AbstractDao<String, Purl> implements PurlDao {
-    
+
+    @Override
+    public Purl findByPurl(String purl) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void save(Purl purl) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Purl purl) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
