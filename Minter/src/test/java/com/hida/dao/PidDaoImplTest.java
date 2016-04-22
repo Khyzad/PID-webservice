e3f0d79c46bdd350ac9dd45bf035d1774290ba34
Khyzad <lljruffin@gmail.com>
2016-04-21 19:34:49 -1000

    Add javadocs to PidDaoImplTest

--- Minter/src/test/java/com/hida/dao/PidDaoImplTest.java
+++ Minter/src/test/java/com/hida/dao/PidDaoImplTest.java
@@ -11,6 +11,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
+ * Tests the functionality of PidDao and ensures that it properly interacts with
+ * Hibernate.
  *
  * @author lruffin
  */
@@ -20,8 +22,10 @@ public class PidDaoImplTest extends EntityDaoImplTest {
     PidDao PidDao;
 
     /**
+     * Retrieves data from an xml file sheet to mock Pids.
      *
-     * @return @throws Exception
+     * @return a data set
+     * @throws Exception
      */
     @Override
     protected IDataSet getDataSet() throws Exception {
@@ -31,7 +35,7 @@ public class PidDaoImplTest extends EntityDaoImplTest {
     }
 
     /**
-     *
+     * Tests to see if PidDao can find an Pid by it's name.
      */
     @Test
     public void testFindByName() {
@@ -42,8 +46,7 @@ public class PidDaoImplTest extends EntityDaoImplTest {
     }
 
     /**
-     * missing javadoc
-     *
+     * Tests to see if Pid can be saved.
      */
     @Test
     public void testSavePid() {
@@ -54,43 +57,48 @@ public class PidDaoImplTest extends EntityDaoImplTest {
         PidDao.savePid(customSample);
 
         Assert.assertEquals(PidDao.findAllPids().size(), 4);
-    }        
+    }
 
     /**
-     * missing javadoc
-     *
+     * Tests to see if all the Pids can be listed and returned.
      */
     @Test
     public void testFindAllPids() {
         int size = PidDao.findAllPids().size();
-        
+
         Assert.assertEquals(size, 2);
     }
-    
+
     /**
-     * 
+     * Tests to see if AutoIds with the same name can be added to the database.
      */
     @Test(expectedExceptions = NonUniqueObjectException.class)
-    public void testNonUniqueObjectExceptionWithAutoId(){
+    public void testNonUniqueObjectExceptionWithAutoId() {
         Pid autoSample1 = getSampleAutoId();
         Pid autoSample2 = getSampleAutoId();
-        
+
         PidDao.savePid(autoSample1);
         PidDao.savePid(autoSample2);
     }
-    
+
     /**
-     * 
+     * Tests to see if CustomIds with the same name can be added to the
+     * database.
      */
     @Test(expectedExceptions = NonUniqueObjectException.class)
-    public void testNonUniqueObjectExceptionWithCustomId(){
+    public void testNonUniqueObjectExceptionWithCustomId() {
         Pid customSample1 = getSampleCustomId();
         Pid customSample2 = getSampleCustomId();
-        
+
         PidDao.savePid(customSample1);
         PidDao.savePid(customSample2);
     }
 
+    /**
+     * returns a sample AutoId.
+     *
+     * @return
+     */
     private Pid getSampleAutoId() {
         Pid sample = new AutoId("", new int[1], "a");
         sample.getName();
@@ -98,6 +106,11 @@ public class PidDaoImplTest extends EntityDaoImplTest {
         return sample;
     }
 
+    /**
+     * Returns a sample CustomId.
+     *
+     * @return
+     */
     private Pid getSampleCustomId() {
         Pid sample = new CustomId("", new int[1], new String[]{"b"});
         sample.getName();
