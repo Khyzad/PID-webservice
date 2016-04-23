fde06501617791e5e87465010d2ec65f41582d17
Khyzad <lljruffin@gmail.com>
2016-04-22 14:16:47 -1000

    Add configuration files for Hibernate testing

---
+++ PURL/src/test/java/com/hida/configuration/HibernateTestConfiguration.java
@@ -0,0 +1,62 @@
+package com.hida.configuration;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Used to create a test environment for test cases.
+ *
+ * @author lruffin
+ */
+@Configuration
+@EnableTransactionManagement
+@ComponentScan({"com.hida.dao"})
+public class HibernateTestConfiguration {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(new String[]{"com.hida.model"});
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean(name = "dataSource")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+        dataSource.setUrl("jdbc:hsqldb:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+        dataSource.setUsername("");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        return properties;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory s) {
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(s);
+        return txManager;
+    }
+}
---
+++ PURL/src/test/java/com/hida/dao/EntityDaoImplTest.java
@@ -0,0 +1,40 @@
+package com.hida.dao;
+
+import javax.sql.DataSource;
+import org.dbunit.database.DatabaseDataSourceConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.operation.DatabaseOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.testng.annotations.BeforeMethod;
+import com.hida.configuration.HibernateTestConfiguration;
+
+/**
+ * A DaoTest class that all Dao test class should extend from. Also adds the
+ * Hsql factory to the database configuration to fix the boolean data type bug.
+ *
+ * @author lruffin
+ */
+@ContextConfiguration(classes = {HibernateTestConfiguration.class})
+public abstract class EntityDaoImplTest extends AbstractTransactionalTestNGSpringContextTests {
+
+    @Autowired
+    DataSource dataSource;
+
+    /**
+     * Setup an in-memory database
+     *
+     * @throws Exception
+     */
+    @BeforeMethod
+    public void setUp() throws Exception {
+        IDatabaseConnection dbConn = new DatabaseDataSourceConnection(dataSource);       
+
+        DatabaseOperation.CLEAN_INSERT.execute(dbConn, getDataSet());
+    }
+
+    protected abstract IDataSet getDataSet() throws Exception;
+
+}
