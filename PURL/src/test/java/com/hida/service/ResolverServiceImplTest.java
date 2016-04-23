57604e059bb9b97715427fe213b3003908c81aaf
Khyzad <lljruffin@gmail.com>
2016-04-22 15:30:06 -1000

    Add mock objects to ResolverServiceImplTest

--- PURL/src/test/java/com/hida/service/ResolverServiceImplTest.java
+++ PURL/src/test/java/com/hida/service/ResolverServiceImplTest.java
@@ -1,13 +1,35 @@
 package com.hida.service;
 
+import com.hida.dao.PurlDao;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.Test;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 
 /**
+ * This class tests the functionality of MinterServiceImpl using Mockito.
  *
  * @author lruffin
  */
 public class ResolverServiceImplTest {
+
+    @Autowired
+    private PurlDao Dao;
+
+    @InjectMocks
+    private ResolverService Service;
+    
+    /**
+     * Sets up Mockito
+     *
+     * @throws Exception
+     */
+    @BeforeClass
+    public void setUpClass() throws Exception {
+        MockitoAnnotations.initMocks(this);        
+    }
 
     @Test
     public void testRetrieveURL() {
