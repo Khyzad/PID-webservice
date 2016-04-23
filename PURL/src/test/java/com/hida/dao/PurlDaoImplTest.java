866c835fad732dd8a917b54d26e10108f91eb78f
Khyzad <lljruffin@gmail.com>
2016-04-22 15:19:15 -1000

    Complete testFindByPurl

--- PURL/src/test/java/com/hida/dao/PurlDaoImplTest.java
+++ PURL/src/test/java/com/hida/dao/PurlDaoImplTest.java
@@ -1,36 +1,58 @@
 package com.hida.dao;
 
+import com.hida.model.Purl;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 
 /**
+ * Tests the functionality of PurlDaoTest and ensures that it properly interacts
+ * with Hibernate.
  *
  * @author lruffin
  */
-public class PurlDaoImplTest extends EntityDaoImplTest{
+public class PurlDaoImplTest extends EntityDaoImplTest {
+    
+    @Autowired
+    private PurlDao Dao;
 
+    /**
+     * Retrieves data from an xml file sheet to mock Pids.
+     *
+     * @return a data set
+     * @throws Exception
+     */
     @Override
     protected IDataSet getDataSet() throws Exception {
         IDataSet dataSet = new FlatXmlDataSet(this.getClass().getClassLoader().
                 getResourceAsStream("Purl.xml"));
-        return dataSet;        
+        return dataSet;
     }
-    
+
+    /**
+     * Tests to see if a purl entity is retrievable
+     */
     @Test
-    public void testFindByPurl(){
+    public void testFindByPurl() {
+        Purl entity1 = Dao.findByPurl("abc123");
+        Purl entity2 = Dao.findByPurl("xyz");
+        Purl entity3 = Dao.findByPurl("null");
+        
+        Assert.assertNotNull(entity1);
+        Assert.assertNotNull(entity2);
+        Assert.assertNull(entity3);        
+    }
+
+    @Test
+    public void testSavePurl() {
         Assert.fail("unimplemented");
     }
 
     @Test
-    public void testSavePurl(){
+    public void testDeletePurl() {
         Assert.fail("unimplemented");
     }
 
-    @Test
-    public void testDeletePurl(){
-        Assert.fail("unimplemented");
-    }
-    
 }
