15c49d919c97efa7230e7eb1e4ae7dc310af02bd
Khyzad <lljruffin@gmail.com>
2016-04-22 20:11:49 -1000

    Add javadoc to PurlDao

--- PURL/src/main/java/com/hida/dao/PurlDao.java
+++ PURL/src/main/java/com/hida/dao/PurlDao.java
@@ -3,6 +3,8 @@ package com.hida.dao;
 import com.hida.model.Purl;
 
 /**
+ * This class is used to define the possible operations that Hibernate can
+ * perform on Purl objects
  *
  * @author lruffin
  */
