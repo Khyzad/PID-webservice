669b88d2d1eadf9d59dabc6417e688eb0db13417
Khyzad <lljruffin@gmail.com>
2016-04-28 21:45:12 -1000

    Create main file

---
+++ Minter/src/main/java/com/hida/Application.java
@@ -0,0 +1,16 @@
+package com.hida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author lruffin
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
