4f4738463440478bfff786b6145fd8340e8e9198
Khyzad <lljruffin@gmail.com>
2016-04-29 20:05:55 -1000

    Add RepositoryConfiguration for testing
    Spring-boot equivalent to Hibernate Configuration and JPA transaction
    manager. Spring-boot bunches up all of these configuration classes into
    the annotations.

---
+++ Minter/src/test/java/com/hida/configuration/RepositoryConfiguration.java
@@ -0,0 +1,22 @@
+package com.hida.configuration;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Spring-boot equivalent to Hibernate Configuration and JPA transaction
+ * manager. Spring-boot bunches up all of these configuration classes into the
+ * annotations.
+ *
+ * @author lruffin
+ */
+@Configuration
+@EnableAutoConfiguration
+@EntityScan(basePackages = {"com.hida.model"})
+@EnableJpaRepositories(basePackages = {"com.hida.dao"})
+@EnableTransactionManagement
+public class RepositoryConfiguration {
+}
