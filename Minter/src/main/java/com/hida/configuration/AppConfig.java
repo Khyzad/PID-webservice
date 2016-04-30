c351e5b9217670bbcf7d7c1be9c9401d7e5a46ec
Khyzad <lljruffin@gmail.com>
2016-04-30 00:23:48 -1000

    Readd AppConfig
    Re-added for the viewrewolver and h2 web console.

---
+++ Minter/src/main/java/com/hida/configuration/AppConfig.java
@@ -0,0 +1,36 @@
+package com.hida.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Acting as a dispatcher.xml, this class resolves requests into view names. 
+ *
+ * @author lruffin
+ */
+@Configuration
+@ComponentScan(basePackages = "com.hida")
+public class AppConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+        
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+}
