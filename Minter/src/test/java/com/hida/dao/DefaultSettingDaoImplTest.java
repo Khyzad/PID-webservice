c0cde038730c9751269e01465331a3eaf93392c2
Khyzad <lljruffin@gmail.com>
2016-05-01 14:46:22 -1000

    Integrate Springboot into DefaultSettingDaoTest
    Integrated for reasons explained before. Aside from that TestNg
    has been replaced with JUnit and Save and find tests has been
    combined into a single test to eliminate redundancy.

--- Minter/src/test/java/com/hida/dao/DefaultSettingDaoImplTest.java
+++ Minter/src/test/java/com/hida/dao/DefaultSettingDaoImplTest.java
@@ -3,14 +3,13 @@ package com.hida.dao;
 import com.hida.configuration.RepositoryConfiguration;
 import com.hida.model.DefaultSetting;
 import com.hida.model.TokenType;
-import org.dbunit.dataset.IDataSet;
-import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.testng.Assert;
-import org.testng.annotations.Test;
 
 /**
  * Tests the functionality of DefaultSettingDaoImpl.
@@ -19,45 +18,25 @@ import org.testng.annotations.Test;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = {RepositoryConfiguration.class})
-public class DefaultSettingDaoImplTest extends EntityDaoImplTest {
+@TestPropertySource(locations = "classpath:testConfig.properties")
+public class DefaultSettingDaoImplTest {
        
-    //@Autowired
     private DefaultSettingRepository DefaultSettingRepo;
     
     @Autowired
     public void setDefaultSettingRepository(DefaultSettingRepository DefaultSettingRepo) {
         this.DefaultSettingRepo = DefaultSettingRepo;
-    }
-
-    /**
-     * Returns a data set that Hibernate assumes to be in persistence.
-     *
-     * @return
-     * @throws Exception
-     */
-    @Override
-    protected IDataSet getDataSet() throws Exception {
-        IDataSet dataSet = new FlatXmlDataSet(this.getClass().getClassLoader().
-                getResourceAsStream("DefaultSetting.xml"));
-        return dataSet;
-    }
-
-    /**
-     * Tests the functionality of DefaultSettingDao save.
-     */
-    @Test
-    public void saveTest() {
-        DefaultSettingRepo.save(getSampleDefaultSetting());
-        DefaultSetting entity = DefaultSettingRepo.findCurrentDefaultSetting();
-        Assert.assertNotNull(entity);
-    }
+    }   
 
     /**
      * Tests to see if DefaultSettingDao can find an entity with an id of 1.
      */
     @Test
-    public void getDefaultSettingTest() {
-        DefaultSetting entity = DefaultSettingRepo.findCurrentDefaultSetting();
+    public void testSaveAndFind() {
+        DefaultSetting sample = getSampleDefaultSetting();                        
+        DefaultSettingRepo.save(sample);
+                
+        DefaultSetting entity = DefaultSettingRepo.findCurrentDefaultSetting();                
         Assert.assertNotNull(entity);
     }
 
@@ -77,5 +56,5 @@ public class DefaultSettingDaoImplTest extends EntityDaoImplTest {
                 true);
 
         return setting;
-    }
+    }        
 }
