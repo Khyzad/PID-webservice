4f30258de9076ef7e11392d2eb2a11e31139412f
Khyzad <lljruffin@gmail.com>
2016-05-01 16:11:54 -1000

    Add console endpoint
    A h2 console was added to view the entries in the database.

--- Minter/src/main/java/com/hida/configuration/AppConfig.java
+++ Minter/src/main/java/com/hida/configuration/AppConfig.java
@@ -1,9 +1,6 @@
 package com.hida.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 import org.h2.server.web.WebServlet;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -18,15 +15,6 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "com.hida")
 public class AppConfig {
 
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/WEB-INF/views/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
-    }
-        
     @Bean
     ServletRegistrationBean h2servletRegistration(){
         ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
