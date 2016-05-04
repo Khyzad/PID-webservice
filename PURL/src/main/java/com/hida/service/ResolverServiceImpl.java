4d4df080b17dfcdac6595fd47a57a9b13b907ba5
Khyzad <lljruffin@gmail.com>
2016-05-03 22:38:49 -1000

    Clean ResolverServiceImpl
    summary:
    - update javadocs
    - replace the class given to logger with ResolverServiceImpl

--- PURL/src/main/java/com/hida/service/ResolverServiceImpl.java
+++ PURL/src/main/java/com/hida/service/ResolverServiceImpl.java
@@ -1,7 +1,6 @@
 package com.hida.service;
 
 import org.apache.log4j.Logger;
-import com.hida.controller.ResolverController;
 import com.hida.model.Citation;
 import com.hida.repositories.CitationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +8,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * ResolverServiceImpl
- *
- * holds functions that allow communication, and communicate with database
- * methods 1.ResolverServiceImpl - sets up conn, specify, serverURL, username,
- * password of database 2.Openconnection 3.closeConnection 4.retrieveUrl -
- * retrieve url requested 5.insertCitation - insert purl, along with its url,
- * who, what, when, erc 6.editUrl - edits url of a specified purl
+ * Allows the performance of CRUD operations on citation objects by by serving
+ * as a middle ground between the database and the controller
  *
  * @author lruffin
  * @author: leland lopez
@@ -23,16 +17,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Service("resolverService")
 @Transactional
 public class ResolverServiceImpl implements ResolverService {
-   
 
     @Autowired
     private CitationRepository CitationRepo;
 
-    final static Logger logger = Logger.getLogger(ResolverController.class);
+    final static Logger logger = Logger.getLogger(ResolverServiceImpl.class);
 
     /**
-     * retrieves url of provided purlid returns url string if successfull, null
-     * if not
+     * Retrieves url of provided purlid
      *
      * @param purl purlid of desired row
      * @return String
@@ -46,7 +38,7 @@ public class ResolverServiceImpl implements ResolverService {
     }
 
     /**
-     * inserts PURL into database returns true if successful, false if not
+     * Inserts PURL into database
      *
      * @param citation Citation to insert into database
      */
@@ -56,8 +48,7 @@ public class ResolverServiceImpl implements ResolverService {
     }
 
     /**
-     * edits url of db row with corresponding purlid. returns true if
-     * successful, false if not
+     * Edits url of db row with corresponding purlid
      *
      * @param purl purlid of desired edited row
      * @param url url that desired row url will be changed to
@@ -69,8 +60,7 @@ public class ResolverServiceImpl implements ResolverService {
     }
 
     /**
-     * deletes db row with corresponding purlid returns true if successful,
-     * false if not
+     * Deletes db row with corresponding purlid
      *
      * @param purl purlid of desired deleted row
      */
@@ -81,7 +71,7 @@ public class ResolverServiceImpl implements ResolverService {
     }
 
     /**
-     * retrieves model of purl_id object returns the respective purl db row.
+     * Retrieves citation corresponding to a particular purl
      *
      * @param purl purlid of desired row that will become Citation
      * @return Citation
