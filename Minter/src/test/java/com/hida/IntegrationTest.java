a083d5a958f6cbbc47a0d57f4ca70fc04a3993f6
Khyzad <lljruffin@gmail.com>
2016-07-12 17:03:03 -1000

    Add minimum annotations for web integration tests

--- Minter/src/test/java/com/hida/IntegrationTest.java
+++ Minter/src/test/java/com/hida/IntegrationTest.java
@@ -13,11 +13,42 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.hida;
+
+import com.hida.configuration.RepositoryConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.BeforeClass;
 
 /**
  *
  * @author lruffin
  */
+@WebAppConfiguration
+@IntegrationTest
+@SpringApplicationConfiguration(classes = {RepositoryConfiguration.class})
+@TestPropertySource(locations = "classpath:testConfig.properties")
+@TestExecutionListeners(inheritListeners = false, listeners = {
+    DependencyInjectionTestExecutionListener.class,
+    DirtiesContextTestExecutionListener.class})
 public class IntegrationTest {
+    @Autowired
+    private WebApplicationContext webAppContext;
     
+    private MockMvc MockedContext;
+
+    @BeforeClass
+    public void setup(){
+        MockedContext = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+    }
 }
