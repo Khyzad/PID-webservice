3c2609fbf98cec5c416496a3c3186fc37b5c714a
Khyzad <lljruffin@gmail.com>
2016-07-14 11:37:14 -1000

    Add public methods to RepositoryService

--- Minter/src/main/java/com/hida/service/RepositoryService.java
+++ Minter/src/main/java/com/hida/service/RepositoryService.java
@@ -17,21 +17,26 @@
  */
 package com.hida.service;
 
+import com.hida.model.DefaultSetting;
+import com.hida.model.Pid;
 import com.hida.repositories.DefaultSettingRepository;
 import com.hida.repositories.PidRepository;
 import com.hida.repositories.UsedSettingRepository;
+import java.io.IOException;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * A service that directly creates transactions with Hibernate
+ *
  * @author lruffin
  */
 @Service("minterService")
 @Transactional
 public class RepositoryService {
-    
+
     @Autowired
     private PidRepository pidRepo_;
 
@@ -40,5 +45,62 @@ public class RepositoryService {
 
     @Autowired
     private DefaultSettingRepository defaultSettingRepo_;
+
+    /**
+     * Creates a set of Pids that are guaranteed to be disjoint from the set of
+     * Pids persisted in the database.
+     *
+     * @param setting The settings the Pids are based off of
+     * @param amount The requested amount of Pids
+     * @return A set of Pids
+     */
+    public Set<Pid> generatePids(DefaultSetting setting, long amount) {
+        return null;
+    }
+
+    /**
+     * Persists a set of Pids in the database. Also adds the Prepend to the 
+     * Pids
+     *
+     * @param setting The settings the Pids are based off of
+     * @param set A set of Pids
+     */
+    public void persistPids(DefaultSetting setting, Set<Pid> set) {
+
+    }
     
+    /**
+     * Returns the difference between the total permutations and the amount of
+     * Pids that were already created using the requested settings.
+     *
+     * @param setting The settings the Pids are based off of
+     * @param amount The requested amount of Pids
+     * @return The amount of permutations remaining
+     */
+    public long getRemainingPermutations(DefaultSetting setting, long amount){
+        return -1;
+    }
+    
+    /**
+     * Updates the CurrentDefaultSetting to match the values in the given
+     * DefaultSetting.
+     *
+     * @param newSetting A DefaultSetting object that contains newly requested
+     * values
+     * @throws IOException Thrown when the file cannot be found
+     */
+    public void updateCurrentSetting(DefaultSetting newSetting) throws IOException {
+        
+    }
+    
+    /**
+     * Initializes the storedSetting_ field by reading its value in the
+     * database. If its null, then it is given initial values.
+     *
+     * @throws IOException Thrown when the file cannot be found
+     */
+    public void initializeStoredSetting() throws IOException {
+        
+    }
+
 }
