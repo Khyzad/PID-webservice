c1abead705e2eeaef65e3ac10f0e65dad72ddde2
Khyzad <lljruffin@gmail.com>
2016-07-14 01:04:06 -1000

    Add test annotations

--- Minter/src/test/java/com/hida/service/CacheServiceTest.java
+++ Minter/src/test/java/com/hida/service/CacheServiceTest.java
@@ -17,11 +17,33 @@
  */
 package com.hida.service;
 
+import com.hida.configuration.RepositoryConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+
 /**
  * A test class designed to test the functionality of CacheService
  *
  * @author lruffin
  */
-public class CacheServiceTest {
+@WebAppConfiguration
+@IntegrationTest
+@SpringApplicationConfiguration(classes = {RepositoryConfiguration.class})
+@TestPropertySource(locations = "classpath:testConfig.properties")
+@TestExecutionListeners(inheritListeners = false, listeners = {
+    DependencyInjectionTestExecutionListener.class,
+    DirtiesContextTestExecutionListener.class})
+public class CacheServiceTest extends AbstractTestNGSpringContextTests {
+    
+    @Autowired
+    private CacheService service_;
+        
 
 }
